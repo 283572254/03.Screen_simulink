415bf954aa1d2e9bf52050d4a7643f2025346626
283572254 <96158209+283572254@users.noreply.github.com>
2025-03-12 21:22:49 +0800

    重新上传

---
+++ 01.UI/output/DefineID.h
@@ -0,0 +1,64 @@
+//创建画面主页面的命令索引ID
+#define  _SCREEN_主页面                                                           0
+
+//创建画面状态栏的命令索引ID
+#define  _SCREEN_状态栏                                                           1
+
+//创建画面阈值页面的命令索引ID
+#define  _SCREEN_阈值页面                                                          2
+
+//创建画面波形页面的命令索引ID
+#define  _SCREEN_波形页面                                                          3
+
+//创建画面电压波形的命令索引ID
+#define  _SCREEN_电压波形                                                          4
+
+//创建画面电流波形的命令索引ID
+#define  _SCREEN_电流波形                                                          5
+
+#define  _BTN_主页面_BUTTON1                                                      1
+
+#define  _BTN_主页面_BUTTON2                                                      2
+
+#define  _BTN_主页面_BUTTON3                                                      3
+
+#define  _BTN_状态栏_BUTTON1                                                      1
+
+#define  _TXT_DIS__状态栏_TEXT_DISPLAY1                                           2
+
+#define  _TXT_DIS__状态栏_TEXT_DISPLAY2                                           3
+
+#define  _TXT_DIS__状态栏_TEXT_DISPLAY3                                           4
+
+#define  _TXT_DIS__状态栏_TEXT_DISPLAY4                                           5
+
+#define  _TXT_DIS__状态栏_TEXT_DISPLAY5                                           6
+
+#define  _TXT_DIS__状态栏_TEXT_DISPLAY6                                           7
+
+#define  _TXT_DIS__状态栏_TEXT_DISPLAY7                                           8
+
+#define  _TXT_DIS__状态栏_TEXT_DISPLAY8                                           9
+
+#define  _BTN_阈值页面_BUTTON1                                                     1
+
+#define  _TXT_DIS__阈值页面_TEXT_DISPLAY1                                          2
+
+#define  _TXT_DIS__阈值页面_TEXT_DISPLAY2                                          4
+
+#define  _BTN_波形页面_BUTTON1                                                     1
+
+#define  _BTN_波形页面_BUTTON2                                                     2
+
+#define  _BTN_波形页面_BUTTON3                                                     3
+
+#define  _BTN_电压波形_BUTTON1                                                     1
+
+#define  _GRAPH_电压波形_GRAPH1                                                    2
+
+#define  _BTN_电压波形_BUTTON2                                                     3
+
+#define  _BTN_电流波形_BUTTON1                                                     1
+
+#define  _GRAPH_电流波形_GRAPH1                                                    2
+
